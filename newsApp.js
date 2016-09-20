4873b0d92cc6f7c817dd7773eabd648a06905c48
An Huynh <An Huynh>
2016-09-19 19:02:06 -0500

    adding my code of newsApp.js

---
+++ newsApp.js
@@ -0,0 +1,53 @@
+//-------------testing user input/data------
+
+var user =
+	{	name: "Jane Doe",
+	    city: 'Hess',
+	    country: 'Germany',
+	    date: "null",
+	    toDoList: ['buy airline tickets', 'save $$', 'get passports'],
+	};
+
+
+
+// --------GOOGLE NEWS API-------------An Huynh-------------
+
+function newsAPI() {
+
+	var countrySearch = "india";
+	//var APIKey	= "b590022778624bc6b7386999057a30ac";
+
+	var queryURL = "https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=" + countrySearch + "&count=10&offset=0&mkt=en-us&safeSearch=Moderate";
+
+	//trying to find the right API URL in order to pull new articles with searchCountry as a topic/perameter
+
+
+$.ajax({
+	url: queryURL, 
+	beforeSend: function(info){
+            // Request headers -- copied from the website
+            info.setRequestHeader("Ocp-Apim-Subscription-Key","f7a5f4560c984fe3b4534359f7b5a1bf");
+        },
+		method: 'GET'
+	})
+	.done(function(response) {
+		var results = response.value;
+		console.log(response);
+
+
+		//appending articles to the newsBox section
+		 for (var i = 0; i < results.length; i++) {
+		 	var newArticle = $('<div class="article">');
+		 	var articleURL = $('<p class="articleURL">').append 
+
+
+		 	newArticle.appendTo($('.newsBox'));
+		 }
+
+}); 
+
+}
+
+$(document).ready(function() {
+	newsAPI();
+});
