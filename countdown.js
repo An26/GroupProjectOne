1b769299d8f2d477efbce914061901ac46d024c8
DVO0808 <damilola.olatayou@utexas.edu>
2016-09-20 00:56:35 -0500

    completed travel info validation

--- countdown.js
+++ countdown.js
@@ -1,75 +1,120 @@
-  $(document).ready(function(){
+ $(document).ready(function() {
 
-//click function for travel info form submit buton 
+    var travelDate;
+    var userName;
+    var travelCountry;
+    var travelCity
 
-$('#travelSubmit').on('click', function(){
+     
 
-//Get the travel date info from form when submit button is clicked 
- var travelDate = $('#arrivalDate').val().trim();
- var userName = $('#userName').val().trim();
- console.log("I've been clicked");
- console.log(travelDate);
+     //click function for travel info form submit buton
+      $('#travelSubmit').on('click', function() {
 
-//Countdown function to find the difference between the current date and the travel date 
+          travelDate = $('#arrivalDate').val().trim();
+          userName = $('#userName').val().trim();
+          travelCountry = $('#travelCountry').val().trim();
+          console.log("I've been clicked");
+          travelCity = $('#f_elem_city').val().trim();
+          console.log("travel date is: " + travelDate);
+          console.log("travel country is: " +travelCountry);
+          console.log("travel city is: " + travelCity);
 
-function myCountdown() {
+        if((!travelDate) || (!userName) || (!travelCountry) || (!travelCity)){
+            return;
+        }
 
+          //Get the travel info from form when submit button is clicked 
+         
 
-   // var travelDate = '11232016';
+          // This is our API Key
+          var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
-    var formatTravelDate = moment(travelDate, 'YYYY-MM-DD');
-    
-    console.log("Travel Date Is: " + travelDate);
-    console.log("Formatted Travel Date Is: " + formatTravelDate);
-    console.log("Type: " + typeof(formatTravelDate));
+          // Here we are building the URL we need to query the database
+          var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + travelCity + "&units=imperial&appid=" + APIKey;
 
-    var now = moment();
+          // Here we run our AJAX call to the OpenWeatherMap API
+          $.ajax({
+              url: queryURL,
+              method: 'GET'
+          })
 
-    var diff = moment.duration(moment(formatTravelDate).diff(moment(now)));
+          // We store all of the retrieved data inside of an object called "response"
+          .done(function(response) {
 
-    console.log(diff);
-  
-    var days = parseInt(diff.asDays()); 
-    
-    var hours = parseInt(diff.asHours()); 
+              // Log the queryURL
+              console.log(queryURL);
 
-    var hours = hours - days*24;  
-   
-    var minutes = parseInt(diff.asMinutes()); 
-  
-    var minutes = minutes - (days*24*60 + hours*60); 
- 
-    var seconds = parseInt(diff.asSeconds()); 
-   
-    var seconds = seconds - (days*24*60*60 + hours*60*60 + minutes*60); 
-  
- 
-    var diffInTimeFromNow ="Countdown To " + userName + "&#39;s Travel: " + days + " " + "Days" + " : " + hours + " " + "Hours " + ": " + minutes + " " + "Minutes " + ": " + seconds + " " + "Seconds";
+              // Log the resulting object
+              console.log(response);
 
-    console.log(diffInTimeFromNow);
+              // Transfer content to HTML
+              $('.city').html("<h3>" + travelCity + '<br>' + " Weather Details</h3>");
+              $(".wind").html("Wind Speed: " + response.wind.speed + " mph");
+              $(".humidity").html("Humidity: " + response.main.humidity + "&#37;");
+              $(".temp").html("Temperature: " + response.main.temp + "&deg;F");
 
-    $("#display").html(diffInTimeFromNow);
+              // Log the data in the console as well
+              console.log("Wind Speed: " + response.wind.speed);
+              console.log("Humidity: " + response.main.humidity);
+              console.log("Temperature (F): " + response.main.temp);
 
-}
-//Placing the countdown function in an interval that causes it to run every one second 
- var myCountdownInterval = setInterval(myCountdown, 1000); 
- $('#arrivalDate').val("");
- $('#userName').val("");
-
-  
-})
-
-
-
+          });
 
 
 
+          //Countdown function to find the difference between the current date and the travel date 
+
+          function myCountdown() {
 
 
+              // var travelDate = '11232016';
+
+              var formatTravelDate = moment(travelDate, 'YYYY-MM-DD');
+
+              //console.log("Travel Date Is: " + travelDate);
+              //console.log("Formatted Travel Date Is: " + formatTravelDate);
+              // console.log("Type: " + typeof(formatTravelDate));
+
+              var now = moment();
+
+              var diff = moment.duration(moment(formatTravelDate).diff(moment(now)));
+
+              //console.log(diff);
+
+              var days = parseInt(diff.asDays());
+
+              var hours = parseInt(diff.asHours());
+
+              var hours = hours - days * 24;
+
+              var minutes = parseInt(diff.asMinutes());
+
+              var minutes = minutes - (days * 24 * 60 + hours * 60);
+
+              var seconds = parseInt(diff.asSeconds());
+
+              var seconds = seconds - (days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60);
+
+
+              var diffInTimeFromNow = userName + " Travels To The " + travelCountry + " In: " + days + " " + "Days" + " : " + hours + " " + "Hours " + ": " + minutes + " " + "Minutes " + ": " + seconds + " " + "Seconds";
+
+              //console.log(diffInTimeFromNow);
+
+              $("#display").html(diffInTimeFromNow);
+
+
+          }
+          //Placing the countdown function in an interval that causes it to run every one second 
+          var myCountdownInterval = setInterval(myCountdown, 1000);
+          $('#arrivalDate').val("");
+          $('#userName').val("");
+          $('#f_elem_city').val("");
+
+
+      })
 
 
 
 
   })
-
 
